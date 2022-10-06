4b3ff46b582d409c741e0283f65e9ae369965915
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-10-06 16:45:20 -0500

    [+] Component: Define state Task Form

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,13 +1,35 @@
+import { useState } from 'react';
+
+
+// Functional Component
 const TaskForm = () => {
+    const [ task, setTask ] = useState({
+        title: '',
+        description: ''
+    });
+
+    const handleChange = e => {
+        // console.log( `${ e.target.name }: ${ e.target.value }` );
+        setTask({
+            ...task,
+            [ e.target.name ]: e.target.value
+        });
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log( 'Save:', task );
+    }
+
     return (
         <section className="section section__task-form">
             <h2>Task Form</h2>
-            <form>
+            <form onSubmit={ handleSubmit }>
                 <div className="form-field">
-                    <input type="text" name="task" placeholder="Name task" />
+                    <input type="text" name="title" placeholder="Name task" onChange={ handleChange } />
                 </div>
                 <div className="form-field">
-                    <textarea name="task" placeholder="Description task"></textarea>
+                    <textarea name="description" placeholder="Description task" onChange={ handleChange }></textarea>
                 </div>
                 <div className="form-field">
                     <button type="submit">Send</button>
